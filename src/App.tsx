ef887e8d1e69f2d8d7e99aead3c85a0e2ef6841c
Lucas Aramendy <yataylucas@gmail.com>
2023-11-19 00:21:09 -0300

    fucking graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import DataStreamer, { ServerRespond } from './DataStreamer';
-import Graph from './Graph';
-import './App.css';
+import React, { Component } from "react";
+import DataStreamer, { ServerRespond } from "./DataStreamer";
+import Graph from "./Graph";
+import "./App.css";
 
 interface IState {
-  data: ServerRespond[],
-  showGraph: boolean,
+  data: ServerRespond[];
+  showGraph: boolean;
 }
 
 class App extends Component<{}, IState> {
@@ -19,41 +19,40 @@ class App extends Component<{}, IState> {
 
   renderGraph() {
     if (this.state.showGraph) {
-      return (<Graph data={this.state.data}/>)
+      return <Graph data={this.state.data} />;
     }
   }
 
-  getDataFromServer() {
-    let x = 0;
-    const interval = setInterval(() => {
-      DataStreamer.getData((serverResponds: ServerRespond[]) => {
-        this.setState({
-          data: serverResponds,
-          showGraph: true,
-        });
+  async getDataFromServer() {
+    try {
+      const serverResponds = await DataStreamer.getData();
+      this.setState({
+        data: serverResponds,
+        showGraph: true,
       });
-      x++;
-      if (x > 1000) {
-        clearInterval(interval);
-      }
-    }, 100);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Bank Merge & Co Task 3
-        </header>
+        <header className="App-header">Bank Merge & Co Task 3</header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button" onClick={() => {this.getDataFromServer()}}>Start Streaming Data</button>
-          <div className="Graph">
-            {this.renderGraph()}
-          </div>
+          <button
+            className="btn btn-primary Stream-button"
+            onClick={() => {
+              this.getDataFromServer();
+            }}
+          >
+            Start Streaming Data
+          </button>
+          <div className="Graph">{this.renderGraph()}</div>
         </div>
       </div>
-    )
+    );
   }
-}
+}    
 
 export default App;
--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -2,6 +2,7 @@ export interface Order {
   price: number,
   size: number,
 }
+
 export interface ServerRespond {
   stock: string,
   top_bid: Order,
@@ -10,21 +11,34 @@ export interface ServerRespond {
 }
 
 class DataStreamer {
-  static API_URL: string = 'http://localhost:8080/query?id=1';
+  static API_URL: string = 'http://localhost:3000/query?id=1';
 
-  static getData(callback: (data: ServerRespond[]) => void): void {
-    const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+  static async getData(): Promise<ServerRespond[]> {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open('GET', DataStreamer.API_URL, true);
 
-    request.onload = () => {
-      if (request.status === 200) {
-        callback(JSON.parse(request.responseText));
-      } else {
-        alert ('Request failed');
-      }
-    }
+      request.onload = () => {
+        if (request.status === 200) {
+          // Imprime la respuesta para depurar
+          console.log('Raw response:', request.responseText);
 
-    request.send();
+          // Intenta limpiar la respuesta antes de analizarla como JSON
+          const cleanedResponse = request.responseText.trim();
+          try {
+            const jsonData = JSON.parse(cleanedResponse) as ServerRespond[];
+            resolve(jsonData);
+          } catch (error) {
+            console.error('Error parsing JSON:', error);
+            reject(new Error('Error parsing JSON'));
+          }
+        } else {
+          reject(new Error('Request failed'));
+        }
+      };
+
+      request.send();
+    });
   }
 }
 
